55fd51b9f403f0c34a924a08960255c60a461c7e
Silvio Giebl <gieblsilvio@outlook.com>
2024-01-11 12:34:08 +0100

    Cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ signing {
 }
 
 nexusPublishing {
-    this.repositories {
+    repositories {
         sonatype {
             nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
             snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
