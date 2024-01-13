f35131e04e26497fa6f725960d0bb0e92c0ca647
Silvio Giebl <gieblsilvio@outlook.com>
2024-01-14 00:55:07 +0100

    Improve OciRegistryHandler link handling

--- src/main/kotlin/io/github/sgtsilvio/oci/registry/OciRegistryHandler.kt
+++ src/main/kotlin/io/github/sgtsilvio/oci/registry/OciRegistryHandler.kt
@@ -109,19 +109,15 @@ class OciRegistryHandler(private val directory: Path) :
         val name = decodeName(segments, segments.lastIndex - 1)
         val reference = segments[segments.lastIndex]
         val manifestsDirectory = resolveRepositoryDirectory(name).resolve("_manifests")
-        val digest = if (':' in reference) {
-            val digest = reference.toOciDigest()
-            if (!Files.exists(manifestsDirectory.resolve("revisions").resolveLinkFile(digest))) {
-                return response.sendNotFound()
-            }
-            digest
+        val linkFile = if (':' in reference) {
+            manifestsDirectory.resolve("revisions").resolveLinkFile(reference.toOciDigest())
         } else {
-            val linkFile = manifestsDirectory.resolve("tags").resolve(reference).resolve("current/link")
-            if (!Files.exists(linkFile)) {
-                return response.sendNotFound()
-            }
-            Files.readAllBytes(linkFile).decodeToString().toOciDigest()
+            manifestsDirectory.resolve("tags").resolve(reference).resolve("current/link")
         }
+        if (!Files.exists(linkFile)) {
+            return response.sendNotFound()
+        }
+        val digest = Files.readAllBytes(linkFile).decodeToString().toOciDigest()
         val dataFile = resolveBlobFile(digest)
         if (!Files.exists(dataFile)) {
             return response.sendNotFound()
@@ -135,10 +131,12 @@ class OciRegistryHandler(private val directory: Path) :
     private fun getOrHeadBlob(segments: List<String>, isGET: Boolean, response: HttpServerResponse): Publisher<Void> {
         val name = decodeName(segments, segments.lastIndex - 1)
         val digest = segments[segments.lastIndex].toOciDigest()
-        if (!Files.exists(resolveRepositoryDirectory(name).resolve("_layers").resolveLinkFile(digest))) {
+        val linkFile = resolveRepositoryDirectory(name).resolve("_layers").resolveLinkFile(digest)
+        if (!Files.exists(linkFile)) {
             return response.sendNotFound()
         }
-        val dataFile = resolveBlobFile(digest)
+        val actualDigest = Files.readAllBytes(linkFile).decodeToString().toOciDigest()
+        val dataFile = resolveBlobFile(actualDigest)
         if (!Files.exists(dataFile)) {
             return response.sendNotFound()
         }
