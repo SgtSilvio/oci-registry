39e4fac7108a27224f554ef2c85213c5f56d8000
Silvio Giebl <gieblsilvio@outlook.com>
2026-08-29 12:57:09 +0200

    Implement Comparable for OciTag

--- src/main/kotlin/io/github/sgtsilvio/oci/registry/OciTagOrDigest.kt
+++ src/main/kotlin/io/github/sgtsilvio/oci/registry/OciTagOrDigest.kt
@@ -7,7 +7,9 @@ internal sealed interface OciTagOrDigest
 
 internal fun String.toOciTagOrDigest() = if (':' in this) toOciDigest() else toOciTag()
 
-internal class OciTag(val name: String) : OciTagOrDigest {
+internal class OciTag(val name: String) : OciTagOrDigest, Comparable<OciTag> {
+
+    override fun compareTo(other: OciTag) = name.compareTo(other.name)
 
     override fun equals(other: Any?) = (this === other) || ((other is OciTag) && (name == other.name))
 
