9bd24781fdfd4320403a272f8ef526b01a71a130
Silvio Giebl <gieblsilvio@outlook.com>
2026-08-31 21:31:38 +0200

    Add OCI tag validation

--- src/main/kotlin/io/github/sgtsilvio/oci/registry/OciTagOrDigest.kt
+++ src/main/kotlin/io/github/sgtsilvio/oci/registry/OciTagOrDigest.kt
@@ -9,6 +9,10 @@ internal fun String.toOciTagOrDigest() = if (':' in this) toOciDigest() else toO
 
 internal class OciTag(val name: String) : OciTagOrDigest, Comparable<OciTag> {
 
+    init {
+        name.validateOciTag()
+    }
+
     override fun compareTo(other: OciTag) = name.compareTo(other.name)
 
     override fun equals(other: Any?) = (this === other) || ((other is OciTag) && (name == other.name))
@@ -19,3 +23,21 @@ internal class OciTag(val name: String) : OciTagOrDigest, Comparable<OciTag> {
 }
 
 internal fun String.toOciTag() = OciTag(this)
+
+/**
+ * Validates `[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}`.
+ */
+private fun String.validateOciTag(): String {
+    if (length !in 1..128) {
+        throw IllegalArgumentException("\"$this\" is not a valid OCI tag: it must be between 1 and 128 characters long.")
+    }
+    if (!this[0].let { it in 'a'..'z' || it in 'A'..'Z' || it in '0'..'9' || it == '_' }) {
+        throw IllegalArgumentException("\"$this\" is not a valid OCI tag: the first character must match `[a-zA-Z0-9_]`.")
+    }
+    for (i in 1..lastIndex) {
+        if (!this[i].let { it in 'a'..'z' || it in 'A'..'Z' || it in '0'..'9' || it in "._-" }) {
+            throw IllegalArgumentException("\"$this\" is not a valid OCI tag: all characters after the first one must match `[a-zA-Z0-9._-]`.")
+        }
+    }
+    return this
+}
